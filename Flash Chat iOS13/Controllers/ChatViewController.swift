c9707c3ce8917c739f007a37bb5e1659851c50a8
youssefsheta <32981194+youssefsheta@users.noreply.github.com>
2022-07-03 17:43:59 +0200

    Add files via upload

---
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ChatViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import FirebaseCore
+import FirebaseAuth
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.hidesBackButton = true
+        
+        
+    }
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+    
+        let firebaseAuth = Auth.auth()
+       do {
+         try firebaseAuth.signOut()
+           navigationController?.popToRootViewController(animated: true)
+       } catch let signOutError as NSError {
+         print("Error signing out: %@", signOutError)
+       }
+    
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+
+platform :ios, '13.0'
+
+target 'Flash Chat iOS13' do
+  use_frameworks!
+pod 'CLTypingLabel' , '~> 0.4.0'
+  # Pods for Flash Chat iOS13
+  pod 'FirebaseAuth'
+  pod 'FirebaseFirestore'
+
+end
